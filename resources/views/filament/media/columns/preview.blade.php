577168304c70a488b4fbbbd81206bc650b8a92cb
lamusiquedemars <ivocore@gmail.com>
2026-08-02 00:09:57 +0200

    fix(media): constrain preview thumbnails

--- resources/views/filament/media/columns/preview.blade.php
+++ resources/views/filament/media/columns/preview.blade.php
@@ -1,11 +1,14 @@
 @php($media = $getRecord())
 
-<div class="overflow-hidden rounded-lg bg-gray-100 dark:bg-gray-800" style="aspect-ratio: 4 / 3; height: auto; min-height: 0">
+<div
+    class="rounded-lg bg-gray-100 dark:bg-gray-800"
+    style="position: relative; display: block; width: 100%; max-width: 100%; min-width: 0; aspect-ratio: 4 / 3; overflow: hidden"
+>
     @if ($media->isImage())
         <img
             src="{{ $media->url() }}"
             alt=""
-            class="h-full w-full object-cover"
+            style="position: absolute; inset: 0; display: block; width: 100%; height: 100%; max-width: 100%; object-fit: cover"
             loading="lazy"
         >
     @elseif ($media->isVideo())
@@ -13,16 +16,16 @@
             <img
                 src="{{ $media->thumbnailUrl() }}"
                 alt=""
-                class="block h-full w-full object-cover"
+                style="position: absolute; inset: 0; display: block; width: 100%; height: 100%; max-width: 100%; object-fit: cover"
                 loading="lazy"
             >
         @else
-            <div class="flex h-full items-center justify-center text-gray-400">
+            <div class="flex items-center justify-center text-gray-400" style="position: absolute; inset: 0">
                 <x-filament::icon icon="heroicon-o-video-camera" class="h-14 w-14" />
             </div>
         @endif
     @else
-        <div class="flex h-full items-center justify-center text-gray-400">
+        <div class="flex items-center justify-center text-gray-400" style="position: absolute; inset: 0">
             <x-filament::icon icon="heroicon-o-document-text" class="h-14 w-14" />
         </div>
     @endif
