91d7e30d4c9e8e893cc5ad7d4c39664f92f169b3
lamusiquedemars <ivocore@gmail.com>
2026-08-21 01:25:28 +0200

    Widen contact form fields

--- app/Modules/Audience/Filament/Resources/AudienceContacts/AudienceContactResource.php
+++ app/Modules/Audience/Filament/Resources/AudienceContacts/AudienceContactResource.php
@@ -64,7 +64,7 @@ class AudienceContactResource extends Resource
     public static function form(Schema $schema): Schema
     {
         return $schema
-            ->columns(2)
+            ->columns(1)
             ->components([
                 TextInput::make('first_name')
                     ->label('Prénom'),
